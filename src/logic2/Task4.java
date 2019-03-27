97f2457b0ad87e0e4b97fcd815997666179b4242
k0sa <kosiorski.tomasz@gmail.com>
2019-03-27 20:59:08 +0100

    logic 2 task 4 finished.

---
+++ src/logic2/Task4.java
@@ -0,0 +1,36 @@
+package logic2;
+
+
+public class Task4 {
+    public static void main(String[] args) {
+        /*
+        Given 3 int values, a b c, return their sum.However,if any of the values is a teen-- in the range 13. .19 inclusive--
+        then that value counts as 0, except 15 and 16 do not count as a teens.Write a separate helper
+        "public int fixTeen(int n) {" that takes in an int value and returns that value fixed for the teen rule.In this way, you
+        avoid repeating the teen code 3 times(i.e."decomposition").Define the helper below and at the same indent level as
+        the main noTeenSum().
+
+        noTeenSum(1, 2, 3) → 6
+        noTeenSum(2, 13, 1) → 3
+        noTeenSum(2, 1, 14) → 3
+        */
+
+        Task4 task = new Task4();
+        System.out.println(task.noTeenSum(2, 13, 1));
+
+
+    }
+
+    public int noTeenSum(int a, int b, int c) {
+        return fixTeen(a) + fixTeen(b) + fixTeen(c);
+
+    }
+
+    public int fixTeen(int n) {
+        if (n == 15 || n == 16) {
+            return n;
+        } else if (n >= 13 && n <= 19) {
+            return 0;
+        } else return n;
+    }
+}
