92eed1273a4d9b6fede231700205cc7e32ba60d8
181537 <tomasz.kosiorski@santander.pl>
2019-09-20 09:39:09 +0200

    Logic2 Task9 done.

---
+++ src/string2/Task8.java
@@ -0,0 +1,25 @@
+package string2;
+
+public class Task8 {
+  public static void main(String[] args) {
+
+    /*
+    We'll say that a String is xy-balanced if for all the 'x' chars in the string, there exists a 'y' char somewhere
+    later in the string. So "xxy" is balanced, but "xyx" is not. One 'y' can balance multiple 'x's.
+    Return true if the given string is xy-balanced.
+
+    xyBalance("aaxbby") → true
+    xyBalance("aaxbb") → false
+    xyBalance("yaaxbb") → false
+    */
+
+    System.out.println(xyBalance("aaxbby"));
+    System.out.println(xyBalance("aaxbb"));
+    System.out.println(xyBalance("yaaxbb"));
+  }
+
+  public static boolean xyBalance(String str) {
+
+    return str.matches("[.*^xy]x*y*");
+  }
+}
