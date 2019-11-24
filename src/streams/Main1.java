d38800a247dffa4a9c64f6ab63e20c7d01e36e6f
k0sa <kosiorski.tomasz@gmail.com>
2019-11-24 22:58:58 +0100

    logic 2 task 8 finished.

--- src/streams/Main1.java
+++ src/streams/Main1.java
@@ -5,13 +5,12 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class Main1 {
-    public static void main(String[] args) {
+  public static void main(String[] args) {
 
-        List<Integer> list = Arrays.asList(1, 22, 93);
+    List<Integer> list = Arrays.asList(1, 22, 93);
+  }
 
-    }
-
-    public List<Integer> rightDigit(List<Integer> nums) {
-        return nums.stream().map(n -> n % 10).collect(Collectors.toList());
-    }
+  public List<Integer> rightDigit(List<Integer> nums) {
+    return nums.stream().map(n -> n % 10).collect(Collectors.toList());
+  }
 }
